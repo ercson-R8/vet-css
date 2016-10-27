8db75740c17c835b8127dcb5c880d49d6e9951da
SATLYTE\ercson <ericson.billedo@upou.edu.ph>
2016-10-27 15:32:25 +0400

    new

--- unit-test/newtest.php
+++ unit-test/newtest.php
@@ -1,1 +1,16 @@
+<?php
 // test new branch
+$slot = [
+    1 => 
+    [
+        101 => [
+            10 => [
+                5 => 27
+            ]
+        ]
+    ] 
+       
+
+];
+echo "<pre>";
+echo print_r( ($slot) );
