83bd51dad33cee3f3c792643063349decdfe596d
ericson billedo <ericson.billedo@upou.edu.h>
2016-11-03 14:14:16 +0400

    to follow,: startPeriod to (startPeriod + numPeriod)-1

--- UnitTest/ga-v1-1.php
+++ UnitTest/ga-v1-1.php
@@ -3,9 +3,6 @@
 require "init-classes.php";
 require "SubjectClasses.php";
 
-
-
-
 // sort trainee group by level
 // the lowest level will be processed first as they would require fewer subjects
 function cmp($a, $b)
@@ -13,14 +10,8 @@ function cmp($a, $b)
     return strcmp($a->getTraineeGroupLevel(), $b->getTraineeGroupLevel());
 }
 
-
-
-
-
 // ---------------------------
 // main code 
-
-
 
 $obj            = createObjects();
 $subject        = $obj[0];
@@ -37,7 +28,8 @@ for($i = 0 ; $i < TOTAL_SLOTS; $i++){
     $slot[$i] = [];
 }
 
-
+$firstPeriod = 0;
+$lastPeriod = 9;
 // $slot[0][] = $subjectClass[0];
 // $slot[0][] = $subjectClass[1];
 // print_r($slot);
@@ -46,34 +38,83 @@ for($i = 0 ; $i < TOTAL_SLOTS; $i++){
 // print_r($slot[0][0]->GetSubjectClassDistributionBlock());
 // print_r($subjectClass[0]);
 
-for ($i = 0; $i < sizeof($subjectClass); $i++){
+
+// per classID (0-7) per subject class
+for ($i = 0; $i < sizeof($subjectClass); $i++){ 
+    //per distBlock( this version uses 1x block or 1 day) w/ 2-3periods{
     for($j = 0; $j <  sizeof($subjectClass[$i]->GetSubjectClassDistributionBlock()); $j++){
-        // print_r ($subjectClass[$i]->GetSubjectClassDistributionBlock());
+        // print_r ($subjectClass[$i]->GetSubjectClassDistributionBlock()[$j]);
+        // number of period for this subject per day
+        $numPeriod = ($subjectClass[$i]->GetSubjectClassDistributionBlock()[$j]);
         $sameDay = false;
         while (!$sameDay){
-            $min = mt_rand(0,9); // start at period zero up to period 9
-            print_r ($min);
-            echo "<br/> -> ";
-            $sameDay = true;
+            // find random (0-9) strating period n
+            // $min = mt_rand($firstPeriod, $lastPeriod-1); // start at period zero up to period 9
+            
+            // check if random period n and n+ is on the same day
+
+
+
+
+            // if ( ( (int) ($min/5) ) ==  ((int) (($min+1)/5) ) ){
+            //     echo "<br/> $min = ";
+            //     print_r ( (int) ($min/5) );
+            //     echo " <> ";
+            //     print_r ((int) (($min+1)/5) );
+                $sameDay = true;
+            //     echo "<br/>";
+            // }
+            
         }
+
+        // per period{ start random n period up n+1 period
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //    $array = array("banana","apple","orange","apple");
 //    print_r(array_search("apple", $array));
 //     $a1=array("red","green","red","yellow");
 //     $a2=array("red");
-
 //     $result=array_intersect($a1,$a2);
 //     print_r($result);
-
 //     print_r(sizeof($result));
-
-
-echo "<br/>";    
-echo "period: ";
-echo ((int) ((0%5)+1));   // int(3)   
-
-echo "<br/>";    
-echo "day: ";
-echo((int) ((0/5)+1));    // float(4)   
+// echo "<br/>";    
+// echo "period: ";
+// echo ((int) ((0%5)+1));   // int(3)   
+// echo "<br/>";    
+// echo "day: ";
+// echo((int) ((0/5)+1));    // float(4)   
