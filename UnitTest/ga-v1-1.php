36f48f5ba852c134db8f73bf223aeeb4c36a7d37
ericson billedo <ericson.billedo@upou.edu.h>
2016-11-03 09:43:36 +0400

    searched some functions to biuld psuedocode.

--- UnitTest/ga-v1-1.php
+++ UnitTest/ga-v1-1.php
@@ -32,14 +32,6 @@ $subjectClass   = $obj[5];
 
 usort($traineeGroup, "cmp");
 
-echo "<br/>";
-
-
-// print_r($preference);
-
-
-
-
 $slot = [];
 for($i = 0 ; $i < TOTAL_SLOTS; $i++){
     $slot[$i] = [];
@@ -57,48 +49,27 @@ print_r($subjectClass[0]);
 for ($i = 0; $i < sizeof($subjectClass); $i++){
     for($j = 0; $j <  sizeof($subjectClass[$i]->GetSubjectClassDistributionBlock()); $j++){
         print_r ($subjectClass[$i]->GetSubjectClassDistributionBlock());
+        
     }
 
 
 }
 
 
+   $array = array("banana","apple","orange","apple");
+   
+   print_r(array_search("apple", $array));
 
 
 
 
 
+$a1=array("red","green","red","yellow");
+$a2=array("red");
+
+$result=array_intersect($a1,$a2);
+print_r($result);
+
+print_r(sizeof($result));
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// if (true) echo print_r($traineeGroup);
-// $arr = array(0 => array('id'=>1,'name'=>"cat 1"),
-//              1 => array(array('id'=>3,'name'=>"cat 2")),
-//              2 => array('id'=>2,'name'=>"cat 2")
-// );
-// $arrIt = new RecursiveIteratorIterator(new RecursiveArrayIterator($arr));
-
-//  foreach ($arrIt as $sub) {
-//     $subArray = $arrIt->getSubIterator();
-//     if ($subArray['name'] === 'cat 1') {
-//         $outputArray[] = iterator_to_array($subArray);
-//     }
-// }
-
-// print_r($outputArray);
--- UnitTest/psuedocode.php
+++ UnitTest/psuedocode.php
@@ -6,8 +6,8 @@ per classID (0-7){ // persubject class
     per distBlock(1x) w/ 2-3periods{
         sameDay = false;
         while !(sameDay){
-            find random (0-9) strating period no.
-            check if random no. n and n+1 is on the same day
+            find random (0-9) strating period n
+            check if random period n and n+1 is on the same day
                 sameDay= true;
         }
         per period{ start random n period up n+1 period
