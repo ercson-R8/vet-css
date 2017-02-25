0fddd5d50558835f16e2386625280ee73df1cc69
SATLYTE\ercson <ericson.billedo@upou.edu.ph>
2017-02-25 19:41:33 +0400

    pop size 50

--- App/Controllers/Timetable/Timetable.php
+++ App/Controllers/Timetable/Timetable.php
@@ -354,7 +354,7 @@ class Timetable {
                 $lowest = $x;
                 $lowest_i = $i;
             }
-            if ($i > 100 or $lowest == 0){
+            if ($i > 50 or $lowest == 0){
                 break;
             }
             $i++;
